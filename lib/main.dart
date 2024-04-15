6b973180e67fac3d27a5a8b26314f881a2ae50a6
KRISH SONI <67964054+krishvsoni@users.noreply.github.com>
2024-04-15 11:00:01 +0530

    title fixed

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DoodleAI',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: const Color(0xff3b3734),
         colorScheme: ColorScheme.fromSeed(
